214303afe5303b9f5141cc9444688cbaab7c2846
Dana-ZK <danakrylova@gmail.com>
2021-05-25 17:23:23 +0300

    first HW

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// let number = 5;
-// const anotherNumber = 2;
-
-// number = 12;
-// console.log(number);
-
-
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 console.log(numberOfFilms);
 
@@ -16,13 +9,6 @@ const personalMovieDB = {
     privat: false,
 };
 
-
-// const questions = {
-//     film: prompt('Один из последних просмотренных фильмов?', ''),
-//     rang: prompt('На сколько оцените его?', ''),
-// }
-
-// personalMovieDB.movies = questions;
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
     b = prompt('На сколько оцените его?', ''),
